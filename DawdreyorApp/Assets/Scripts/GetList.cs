60085d873ae8fac229f08c4ea1bec1b19a6e4a64
Askbackwards <jjbaddude@hotmail.com>
2016-05-16 18:59:22 -0400

    First Upload

---
+++ DawdreyorApp/Assets/Scripts/GetList.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetList : MonoBehaviour {
+
+	private TextAsset txt;
+	private string content;
+
+	// Use this for initialization
+	void Start () {
+		//Read Text File and get Objects
+		txt = (TextAsset)Resources.Load("Objects.txt", typeof(TextAsset));
+		content = txt.text;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
