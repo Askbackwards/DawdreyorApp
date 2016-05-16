a49b46bc05c3f4e7cca35184c0af534a69c0c38c
Askbackwards <jjbaddude@hotmail.com>
2016-05-16 19:15:29 -0400

    CODE

--- DawdreyorApp/Assets/Scripts/GetList.cs
+++ DawdreyorApp/Assets/Scripts/GetList.cs
@@ -5,12 +5,16 @@ public class GetList : MonoBehaviour {
 
 	private TextAsset txt;
 	private string content;
+	private int[] headerPos;
 
 	// Use this for initialization
 	void Start () {
-		//Read Text File and get Objects
+		//load Text File
 		txt = (TextAsset)Resources.Load("Objects.txt", typeof(TextAsset));
 		content = txt.text;
+
+		//Search it for headers
+		//Use IndexOf(char, Int32) int 32 being starting pos
 	}
 	
 	// Update is called once per frame
