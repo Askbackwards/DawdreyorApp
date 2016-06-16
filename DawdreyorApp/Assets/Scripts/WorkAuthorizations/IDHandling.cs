401bd4dbbb06aa9e7671fb3da7132c95527517e4
Askbackwards <jjbaddude@hotmail.com>
2016-06-16 11:38:41 -0400

    Added WorkAuth and got selectionscreen done

---
+++ DawdreyorApp/Assets/Scripts/WorkAuthorizations/IDHandling.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IDHandling : MonoBehaviour {
+
+	private string ID;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+
+	public void SetID(string WONum) {
+		ID = WONum;
+	}
+
+	public void SetPPWithID() {
+		PlayerPrefs.SetString ("WOID", ID);
+	}
+}
---
+++ DawdreyorApp/Assets/Scripts/WorkAuthorizations/LoadList.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LoadList : MonoBehaviour {
+
+	public GameObject WONum, contentPane, loadingScreen;
+
+	private string listURL, theList;
+	private string[] listArray;
+	private bool canFinish;
+
+	void Start () {
+		//Initialize
+		listURL = "https://dl.dropboxusercontent.com/s/9lnd15w7mccer0v/WOList.txt";
+
+		//Get list
+		StartCoroutine(GetTheList());
+
+	}
+
+	void Update () {
+		if (canFinish)
+			Finish ();
+	}
+
+	//Coroutine to retrieve text files
+	private IEnumerator GetTheList() {
+		WWW listWWW = new WWW (listURL);
+		yield return listWWW;
+		theList = listWWW.text;
+		canFinish = true;
+	}
+
+	private void Finish() {
+		//Split dat text
+		listArray = theList.Split(",".ToCharArray());
+
+		//Create the new object
+		for (int i = 0; i < listArray.Length; i++) {
+			GameObject tempObj = (GameObject)Instantiate (WONum);
+			tempObj.GetComponentInChildren<Text> ().text = listArray[i];
+			tempObj.transform.SetParent (contentPane.transform);
+			tempObj.transform.localScale = new Vector3 (1, 1, 1);
+			tempObj.GetComponent<RectTransform> ().position = WONum.GetComponent<RectTransform> ().position - new Vector3(0,1 * i,0);
+			tempObj.GetComponent<IDHandling> ().SetID (listArray [i]);
+			tempObj.SetActive (true);
+		}
+
+		loadingScreen.SetActive (false);
+		canFinish = false;
+	}
+}
